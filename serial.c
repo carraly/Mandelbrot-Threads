b1fe3fb0e98409d2b5b05fb5ddfa36f15baa8d71
carraly <vcarraly@gmail.com>
2026-08-28 21:10:04 -0300

    Resolvido, era só não usar o valor exato dos limites e um = que eu esqueci de colocar

--- serial.c
+++ serial.c
@@ -5,23 +5,15 @@
 void mandelbrot_serial(int width, int height, int num_interactions) {
     // Trocar por lista dinâmica dps
     double vertical_positions[height];
-    double increment = (TOP_LIMIT-BOTTOM_LIMIT)/(height-1);
+    double increment = (TOP_LIMIT-BOTTOM_LIMIT)/(height);
     for (int i = 0; i < height; i++) {
-        if (i == (height-1)) {
-            vertical_positions[height-1] = BOTTOM_LIMIT;
-        }else {
-            vertical_positions[i] = TOP_LIMIT - (i * increment);
-        }
+        vertical_positions[i] = TOP_LIMIT - (i * increment);
     }
     
     double horizontal_positions[width];
-    increment = (RIGHT_LIMIT-LEFT_LIMIT)/(width-1);
+    increment = (RIGHT_LIMIT-LEFT_LIMIT)/(width);
     for (int i = 0; i < width; i++) {
-        if (i == (width-1)) {
-            horizontal_positions[width-1] = RIGHT_LIMIT;
-        }else {
-            horizontal_positions[i] = LEFT_LIMIT + (i * increment);
-        }
+        horizontal_positions[i] = LEFT_LIMIT + (i * increment);
     }
     
     FILE* file = fopen("mandelbrot_login_serial.pgm", "w");
@@ -41,13 +33,13 @@ void mandelbrot_serial(int width, int height, int num_interactions) {
             double z_real = 0.0;
             double z_imag = 0.0;
 
-            while (cont < num_interactions && ((z_real * z_real) + (z_imag * z_imag)) < 4) {
+            while (cont < num_interactions && ((z_real * z_real) + (z_imag * z_imag)) <= 4) {
                 double temp_real = ((z_real * z_real) - (z_imag * z_imag)) + c_real;
                 z_imag = (2 * z_real * z_imag) + c_imag;
                 z_real = temp_real;
                 cont++;
             }
-            int normalized_cont = (int)(cont * normalize + 0.5); // 0.5 para arredondar para cima
+            int normalized_cont = (int)(cont * normalize + 0.4); // 0.4 para arredondar para cima
             fprintf(file, "%d ", normalized_cont);
         }
     fprintf(file, "\n");
